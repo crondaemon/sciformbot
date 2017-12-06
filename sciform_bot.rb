71ca3ad8b084fd5d332eba6f28166eb61990e548
Dario Lombardo <lomato@gmail.com>
2017-12-06 22:07:22 +0100

    Change bot to support groups.

--- sciform_bot.rb
+++ sciform_bot.rb
@@ -4,11 +4,15 @@ require 'html_to_plain_text'
 require 'rest-client'
 
 CHATSFILE = 'chatsfile.yml'
-CONTACTSFILE = 'contacts.yml'
 PAGESFILE = 'pagesfile.yml'
 
 def load_chats
 	$chats = YAML.load_file(CHATSFILE)
+	if $chats
+		$bot.logger.info("Chats loaded: #{$chats.map{|id,chat| "#{id}-#{chat[:title]}"}}")
+	else
+		$chats = {}
+	end
 end
 
 def save_chats
@@ -16,41 +20,38 @@ def save_chats
 end
 
 $commands = Telegram::Bot::Types::ReplyKeyboardMarkup.new(resize_keyboard: true,
-	keyboard: [
-		['pagine' ,'watchers'],
-		['aiuto', Telegram::Bot::Types::KeyboardButton.new(text: 'num. tel.', request_contact: true)],
-])
+	keyboard: [['/pagine']])
 
 def send_message(chat_id, text)
+	if !$chats.dig(chat_id, :permit)
+		$bot.api.send_message(chat_id: chat_id, text: "Questa chat non e' autorizzata!")
+		return
+	end
+
 	begin
 		$bot.api.send_message(chat_id: chat_id, reply_markup: $commands, parse_mode: 'Markdown', text: text)
-	rescue Telegram::Bot::Exceptions::ResponseError
+	rescue Telegram::Bot::Exceptions::ResponseError => e
 		$bot.logger.info("The chat #{chat_id} is no longer responding. Removing.")
+		$bot.logger.debug("Chat reported: #{e}")
 		$chats.delete(chat_id)
 		save_chats
 	end
 end 
 
-def process_start(message)
-	text = "Ciao #{message.from.first_name}, io sono il bot SciForm. "
+def process_start(chat)
+	text = "Ciao, io sono il bot SciForm. "
 	text << "Ti informero' su modifiche alle mie pagine monitorate. Solo gli utenti autorizzati "
 	text << "possono accedere ai miei servizi. Devi mandarmi il tuo numero per ricevere le mie "
 	text << "notifiche, cosi' posso verificare se sei autorizzato."
-	send_message(message.chat.id, text)
+	send_message(chat.id, text)
 end
 
-def verify_contact(message)
-	contacts = YAML.load_file(CONTACTSFILE).keys.map(&:to_s)
-	if contacts.include?(message.contact.phone_number)
-		$chats[message.chat.id] = { name: "#{message.from.first_name} #{message.from.last_name}" }
-		save_chats
-		send_message(message.chat.id, "Ok, sei autorizzato. Da questo momento riceverai le notifiche.")
-	else
-		send_message(message.chat.id, "Mi spiace, non sei un utente autorizzato.")
-	end
+def process_help(chat)
+	send_message(chat.id, "Io monitoro alcune pagine di sciform e ti avviso se ci sono modifiche." +
+		" usa il comando /pagine per vedere quali pagine sto monitorando.")
 end
 
-def process_pages(message)
+def process_pages(chat)
 	return if !$pages
 	text = "Le mie pagine monitorate sono:\n\n"
 
@@ -58,39 +59,11 @@ def process_pages(message)
 		text << "#{i}) [#{page['label']}](#{page['url']})\n"
 	end
 
-	send_message(message.chat.id, text)
+	send_message(chat.id, text)
 end
 
-def process_help(message)
-	text = "C'e' poco da sapere. Io controllo periodicamente alcune pagine e ti notifico se ci sono cambiamenti."
-	text << "Ti mandero' anche il link alla pagina, per potervi cliccare."
-	text << "Ci sono comandi aggiuntivi disponibili: scrivendo / ti verranno elencati."
-	send_message(message.chat.id, text)
-end
-
-def process_watchers(message)
-	send_message(message.chat.id, "Attualmente ci sono #{$chats.size} utenti in osservazione.")
-end
-
-def send_mail(from, text)
-	`echo "[#{from}]: #{text}" | mail -s "Messaggio da SciFormBot" lomato@gmail.com`
-end
-
-def process_other(message)
-	case $chats[message.chat.id]['status']
-	when :bug
-		header = "[BUG] "
-		send_message(message.chat.id, "Bug segnalato, grazie.")
-	when :feature
-		header = "[FEATURE] "
-		send_message(message.chat.id, "La richiesta e' stata inoltrata. Il mio padrone vedra' cosa puo' fare.")
-	when nil
-		header = ""
-		send_message(message.chat.id, "Mi spiace, non ho capito.")
-	end
-	$chats[message.chat.id]['status'] = nil
-	save_chats
-	send_mail("#{message.from.first_name} #{message.from.last_name}", header + message.text)
+def process_hello(chat)
+	send_message(chat.id, "Ciao :-)")
 end
 
 def telegram_loop
@@ -101,29 +74,23 @@ def telegram_loop
 	load_chats
 
 	$bot.listen do |message|
+		chat = message.chat
+
+		if !$chats[chat.id]
+			$chats[chat.id] = chat.to_h 
+			save_chats
+		end
+
 		case
-		when message.contact
-			verify_contact(message)
-		when message.text == '/start'
-			process_start(message)
 		when !message.text
-			send_message(message.chat.id, "Non so gestire questo contenuto.")
-		when message.text.start_with?('/bug')
-			send_message(message.chat.id, "Stai segnalando un bug. Spiega il problema.")
-			$chats[message.chat.id]['status'] = :bug
-			save_chats
-		when message.text.start_with?('/feature')
-			send_message(message.chat.id, "Stai richiedendo una nuova feature. Descrivila brevemente.")
-			$chats[message.chat.id]['status'] = :feature
-			save_chats
-		when message.text == 'watchers'
-			process_watchers(message)
-		when message.text == '/help' || message.text.downcase == 'aiuto'
-			process_help(message)
-		when message.text.downcase == 'pagine'
-			process_pages(message)
-		else
-			process_other(message)
+		when message.text == '/start'
+			process_start(chat)
+		when message.text == '/aiuto' || message.text.downcase == 'aiuto'
+			process_help(chat)
+		when message.text == '/pagine' || message.text.downcase == 'pagine'
+			process_pages(chat)
+		when message.text.include?('sciformbot')
+			process_hello(chat)
 		end
 	end
 end
@@ -166,7 +133,7 @@ def send_admin_message
 	Dir.glob("*.admin").each do |file|
 		text = File.read(file)
 		$chats.each do |chat_id,chat|
-			$bot.logger.info("Sending content of #{file} to #{chat['user']}")
+			$bot.logger.info("Sending content of #{file} to #{chat['name']}")
 			send_message(chat_id, text)
 		end
 		FileUtils.rm(file)
@@ -175,9 +142,9 @@ end
 
 def pages_loop
 	load_pages
+	minute = 60
 	while true
 		next if !$bot
-		send_admin_message
 		$pages.each do |page|
 			send_chats_action(:typing)
 			$bot.logger.debug("Checking #{page['label']}")
@@ -194,7 +161,7 @@ def pages_loop
 				notify_users(page)
 			end 
 		end
-		sleep 60
+		sleep ENV['SLEEP_TIME'] ? ENV['SLEEP_TIME'].to_i : 20 * minute
 	end
 end
 
@@ -222,6 +189,17 @@ threads << Thread.new do
 			pages_loop
 		rescue => e
 			$bot.logger.error("Error in pages loop: #{e}")
+			log_lines(e.backtrace)
+		end
+	end
+end
+threads << Thread.new do
+	while true
+		begin
+			send_admin_message
+			sleep 5
+		rescue => e
+			$bot.logger.error("Error in admin message loop: #{e}")
 			log_lines(e.backtrace)
 		end
 	end
