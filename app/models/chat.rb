9f612ec998a081d273705cb9695e4b2e7da2deda
Dario Lombardo <lomato@gmail.com>
2017-12-08 22:30:35 +0100

    clean up chat hash.

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -5,6 +5,10 @@ class Chat < ApplicationRecord
 		h.delete(:id)
 		h[:chat_type] = h[:type]
 		h.delete(:type)
+		h.delete(:photo)
+		h.delete(:description)
+		h.delete(:invite_link)
+		h.delete(:pinned_message)
 		h
 	end
 
