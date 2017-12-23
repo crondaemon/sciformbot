66b8d78611faa11dd27426aa83054370df573842
Dario Lombardo <lomato@gmail.com>
2017-12-23 16:49:33 +0100

    db: restyle the db.

--- db/migrate/01_create_chats.rb
+++ db/migrate/01_create_chats.rb
@@ -1,16 +1,14 @@
 class CreateChats < ActiveRecord::Migration[5.1]
   def change
     create_table :chats do |t|
+      t.timestamps
       t.integer :chat_id
       t.string :chat_type
       t.string :title
       t.string :username
       t.string :first_name
       t.string :last_name
-      t.boolean :all_members_are_admins
       t.boolean :permit
-
-      t.timestamps
     end
   end
 end
--- db/migrate/02_create_pages.rb
+++ db/migrate/02_create_pages.rb
@@ -1,12 +1,11 @@
 class CreatePages < ActiveRecord::Migration[5.1]
   def change
     create_table :pages do |t|
+      t.timestamps
       t.string :url
       t.string :label
       t.string :md5
       t.integer :bytes
-
-      t.timestamps
     end
   end
 end
