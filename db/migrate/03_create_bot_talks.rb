0f9aee659a56b328b7da8a488ba9be39204a954e
Dario Lombardo <lomato@gmail.com>
2017-12-23 19:27:27 +0100

    bottalk: fix typo.

--- db/migrate/03_create_bot_talks.rb
+++ db/migrate/03_create_bot_talks.rb
@@ -1,7 +1,7 @@
 class CreateBotTalks < ActiveRecord::Migration[5.1]
   def change
     create_table :bot_talks do |t|
-      t.string :sentece
+      t.string :sentence
       t.boolean :sent
 
       t.timestamps
