4f152695d9389968d2bddfe26e28fea56991ac0c
Dario Lombardo <lomato@gmail.com>
2017-12-23 20:23:10 +0100

    bottalk: set default.

--- db/migrate/03_create_bot_talks.rb
+++ db/migrate/03_create_bot_talks.rb
@@ -2,7 +2,7 @@ class CreateBotTalks < ActiveRecord::Migration[5.1]
   def change
     create_table :bot_talks do |t|
       t.string :sentence
-      t.boolean :sent
+      t.boolean :sent, default: false
 
       t.timestamps
     end
