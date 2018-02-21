77b7c098ecb71cd068a8cd35ef048a1dc69eb1af
Dario Lombardo <lomato@gmail.com>
2018-02-21 21:56:12 +0100

    chat: explicit select fields instead of deleting them.

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -4,15 +4,8 @@ class Chat < ApplicationRecord
 	def self.adapt(chat)
 		h = chat.to_h
 		h[:chat_id] = h[:id]
-		h.delete(:id)
 		h[:chat_type] = h[:type]
-		h.delete(:type)
-		h.delete(:photo)
-		h.delete(:description)
-		h.delete(:invite_link)
-		h.delete(:pinned_message)
-		h.delete(:all_members_are_administrators)
-		h
+		h.slice(:chat_id, :chat_type, :title, :username, :first_name, :last_name)
 	end
 
 	def ref
